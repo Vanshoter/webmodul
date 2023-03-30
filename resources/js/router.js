6c4596251090c34ccb31dc3eff285aab01806c25
Vanshoter <megafun396@gmail.com>
2023-03-31 01:36:59 +0400

    1

---
+++ resources/js/router.js
@@ -0,0 +1,18 @@
+import { processIf } from "@vue/compiler-core";
+import { createRouter, createWebHistory } from "vue-router";
+import Container from "./views/layuot/Container";
+
+const routes = [
+    {
+        path: "/",
+        name: Container,
+        component: Container,
+    },
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(process.env.BASE_URL),
+});
+
+export default router;
---
+++ resources/js/views/layuot/Container.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>vue</h1>
+</template>
+
+<script>
+export default {
+    name: "Container",
+};
+</script>
+
+<style scoped></style>
